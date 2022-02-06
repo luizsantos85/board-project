9c9db8dafcde792eff9dbfea8e92535efbf4bf0c
Luiz Henrique Santos <luiz.santos85@gmail.com>
2022-02-06 16:53:44 -0300

    config inicial

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from "../styles/styles.module.scss";
+
+export default function Home() {
+   return (
+      <div>
+         <h1 className={styles.title}>
+            Primeiro projeto com <span>NEXTJS</span>
+         </h1>
+      </div>
+   );
+}
