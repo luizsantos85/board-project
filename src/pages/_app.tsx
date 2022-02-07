0acc8d032d4327e65af6ed3512488dda01e6c7a5
Luiz Henrique Santos <luiz.santos85@gmail.com>
2022-02-06 23:05:27 -0300

    implementacao pagina home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
+import '../styles/globalStyle.scss'
+import {Header} from '../components/Header';
+
 import {AppProps} from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-   return <Component {...pageProps} />;
+   return (
+      <>
+         <Header/>
+         <Component {...pageProps} />
+      </>
+   );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,36 @@
+import Head from "next/head";
 import styles from "../styles/styles.module.scss";
 
 export default function Home() {
    return (
-      <div>
-         <h1 className={styles.title}>
-            Primeiro projeto com <span>NEXTJS</span>
-         </h1>
-      </div>
+      <>
+         <Head>
+            <title>Board - Organizador de Tarefas</title>
+         </Head>
+
+         <main className={styles.contentContainer}>
+            <img src="/images/board-user.svg" alt="image_ferramenta" />
+
+            <section className={styles.callToAction}>
+               <h1>
+                  Uma ferramenta para o seu dia a dia, Escreva, planeje e
+                  organize-se...
+               </h1>
+               <p>
+                  <span>100% gratuito</span> e online
+               </p>
+            </section>
+
+            <div className={styles.donatersContainer}>
+                  <h1>Apoiadores:</h1>
+               <div className={styles.donaters}>
+                  <img
+                     src="https://sujeitoprogramador.com/steve.png"
+                     alt="image_apoiador"
+                  />
+               </div>
+            </div>
+         </main>
+      </>
    );
 }
