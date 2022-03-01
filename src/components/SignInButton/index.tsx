598fc5edaab3cd2f4c037c4345653a5de14e79c8
Luiz Henrique Santos <luiz.santos85@gmail.com>
2022-02-28 23:20:23 -0300

    implementacao de login com next auth + github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,27 @@
+import { signIn, signOut, useSession } from "next-auth/client";
 import styles from "./styles.module.scss";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 
 export function SignInButton() {
-   const session = true;
+   const [session] = useSession();
 
    return session ? (
-      <button type="button" className={styles.signInButton} onClick={() => {}}>
-         <img
-            src="https://sujeitoprogramador.com/steve.png"
-            alt="foto_do_usuario"
-         />
-         Olá Luiz
+      <button
+         type="button"
+         className={styles.signInButton}
+         onClick={() => signOut()}
+      >
+         <img src={session.user.image} alt="foto_do_usuario" />
+         Olá {session.user.name}
          <FiX color="#737380" className={styles.closeIcon} />
       </button>
    ) : (
-      <button type="button" className={styles.signInButton} onClick={() => {}}>
+      <button
+         type="button"
+         className={styles.signInButton}
+         onClick={() => signIn("github")}
+      >
          <FaGithub color="#FFB800" />
          Entrar com github
       </button>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
-import '../styles/globalStyle.scss'
-import {Header} from '../components/Header';
-
-import {AppProps} from 'next/app'
+import { Header } from "../components/Header";
+import { AppProps } from "next/app";
+import { Provider as NextAuthProvider } from "next-auth/client";
+import "../styles/globalStyle.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
    return (
-      <>
-         <Header/>
+      <NextAuthProvider session={pageProps.session}>
+         <Header />
          <Component {...pageProps} />
-      </>
+      </NextAuthProvider>
    );
 }
 
